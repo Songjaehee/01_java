25cb67bb00503be8a9a7ea349b0c7eb5064cea0d
jaehee <sally0723s@gmail.com>
2024-04-11 18:29:10 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+    public static void main(String[] args){
+
+        //정수형 리터럴
+        System.out.println(123);
+
+        //실수형 리터럴
+        System.out.println(3.141592F);
+
+        //문자형 리터럴
+        System.out.println('A');
+
+        //문자열형 리터럴
+        System.out.println("hello world");
+
+        //논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
